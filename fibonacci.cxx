f0cda45af0934d965f7a015a760b3034f012712e
Camilla Willim <camilla@supernova.tp1.uni-duesseldorf.de>
2015-11-03 16:49:40 +0100

    Getan

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,23 @@
+# include <iostream>
 
+using namespace std;
+
+int main(){
+ int f1=0;
+ int f2=1;
+ int f;
+ int N;
+ cin >> N;
+ for(int i=0; i<N;i++){
+    
+     f=f1+f2;
+     f2=f1;
+     f1=f;
+     
+
+     
+    
+ }
+ cout << f << endl;
+ return 0;
+}
